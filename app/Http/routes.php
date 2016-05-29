e72fc2040b9e2588176163daf62ca2dc47372c6c
Malik Ghanim <malikghanim@gmail.com>
2016-05-29 04:46:51 +0000

    Publish Admin LTE layout, update route and update Auth\AuthController.php (update , add )

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,3 +31,20 @@ Route::group(['prefix' => 'api', 'namespace' => 'API'], function () {
         require config('infyom.laravel_generator.path.api_routes');
     });
 });
+
+
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+Route::get('/dashboard', 'HomeController@index');
